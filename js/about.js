1d7be9c9ffd48b0698c0e9d8c2c22cc1a17515f6
Wahidullah Shadab <shadabwahidullah@gmail.com>
2021-09-16 03:53:59 +0430

    Add speakers dynamiclly using JS object

--- js/about.js
+++ js/about.js
@@ -2,12 +2,12 @@ const menubtn = document.getElementById('menu-icon');
 const closebtn = document.getElementById('close-icon');
 const popupMenu = document.querySelector('.popup-menu');
 
-menubtn.addEventListener('click',() => {
-    popupMenu.classList.toggle('hidden');
-    menubtn.classList.toggle('hidden')
+menubtn.addEventListener('click', () => {
+  popupMenu.classList.toggle('hidden');
+  menubtn.classList.toggle('hidden');
 });
 
-closebtn.addEventListener('click',() => {
-    popupMenu.classList.toggle('hidden');
-    menubtn.classList.toggle('hidden')
+closebtn.addEventListener('click', () => {
+  popupMenu.classList.toggle('hidden');
+  menubtn.classList.toggle('hidden');
 });
